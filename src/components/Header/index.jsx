256ed8ad9612a8bfd95131c5c2e052545b644b88
Shukuruov2009 <shukurovfayzullo83@gmail.com>
2024-03-13 19:04:52 +0500

    added

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import "./header.css"
+
+export const Header = () => {
+    return (
+        <>
+            <header className="header">
+                <div className="container">
+                    <div className="header-inner">
+                        
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
